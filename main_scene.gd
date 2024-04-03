a48dc23b0e1dbc16869caa42920add65c97dfa67
Planeptunia <sergienkov03@gmail.com>
2024-04-03 16:28:20 +0300

    working modular upgrades

--- main_scene.gd
+++ main_scene.gd
@@ -1,70 +1,64 @@
 extends Node2D
 var money = 0
-var timer
-# Множитель увелечения цен строений и заробатка в секунду за одну покупку
-# new_price = last_price * multiplier^n+1 | n = 1
-var multiplier = 1.10
-var click = 1
-var n = 1
-var money_per_second = 0
-var mnp = 0
-# Цены строений
-var price1 = 15
-var price2 = 100
-var price3 = 500
-var price4 = 3000
-var price5 = 10000
-var price6 = 40000
-var price7 = 200000
-var price8 = 1666666
-var price9 = 123456789
-var price10 = 75000000000
-var price = 0
-func init():
-	timer = Timer.new()
-	add_child(timer)
-	timer.autostart = true
-	timer.wait_time = 1
-	timer.connect("timeout", self, "_timeout")
 
-func _timeout():
-	money += money_per_second
+var upgradeButton = preload("res://upgrade_button.tscn")
+
+var upgrades
+var upgradesList = {}
+
+func _init():
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	update_money()
+	
+	upgrades = FileAccess.open("res://upgrades.json", FileAccess.READ)
+	upgrades = upgrades.get_as_text()
+	var json = JSON.new()
+	upgrades = json.parse_string(upgrades)
+	for upgrade in upgrades['upgrades'].keys():
+		var newUpgrade = upgradeButton.instantiate()
+		upgradesList[upgrade] = newUpgrade
+		upgradesList[upgrade].id = upgrade
+		if 'price_multiplier' in upgrades['upgrades'][upgrade].keys():
+			print(upgrades['upgrades'][upgrade].keys())
+			upgradesList[upgrade].price_multiplier = upgrades['upgrades'][upgrade]['price_multiplier']
+		if 'mps_multiplier' in upgrades['upgrades'][upgrade].keys():
+			print(upgrades['upgrades'][upgrade].keys())
+			upgradesList[upgrade].mps_multiplier = upgrades['upgrades'][upgrade]['mps_multiplier']
+		upgradesList[upgrade].baseCost = upgrades['upgrades'][upgrade]['price']
+		upgradesList[upgrade].change_icon(upgrades['upgrades'][upgrade]['icon'])
+		upgradesList[upgrade].change_price(upgrades['upgrades'][upgrade]['price'])
+		upgradesList[upgrade].change_name(upgrades['upgrades'][upgrade]['name'])
+		upgradesList[upgrade].clicked.connect(on_pressed)
+		$Control/scroll_container/Control.add_child(newUpgrade)
+	$Control/scroll_container/Control.custom_minimum_size = Vector2(635, (upgradesList.size() * 110) + 110)
 
+func on_pressed(upgradeId):
+	var upgrade_price = upgradesList[upgradeId].get_info().y
+	if money >= upgrade_price:
+		upgradesList[upgradeId].change_amount(upgradesList[upgradeId].get_info().x + 1)
+		money -= upgrade_price
+		update_money()
+	else: print("no money")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-class towers:
-	var multiplier = 1.10
-	var n = 1
-	var money_per_second
-	func _init_(price, mnp):
-		self.price = price
-		self.mnp = mnp
-	func when_pressed():
-		self.price = self.price * multiplier ** (n + 1)
-		money_per_second += self.mnp 
-		self.mnp = self.mnp * multiplier ** (n + 1)
-
-var ls = [0, 1, 44, 5, 6]
-var prices = [100, 200, 300, 400, 500]
-var mps = [1, 2, 3, 4, 5]
-var monps = 0
-func _when_pressed(upgrade):
-	for i in ls:
-		if money < prices[i]:
-			pass
-		n = ls[i]
-		prices[i] = prices[i] * multiplier ** (n + 1)
-		monps += mps[i] * n
-
+	
+func _physics_process(delta):
+	var mps = 0
+	for upgrade in upgradesList:
+		var upgrade_amount = upgradesList[upgrade].get_info().x
+		mps += (upgrade_amount * upgrades['upgrades'][upgrade]['mps']) * upgradesList[upgrade].mps_multiplier
+	money += mps / 60
+	update_money()
 
 func _on_fish_click_pressed():
-	money += click * multiplier
-	get_node("Control/Control2/money").text = str(money)
-
+	money += 1
+	update_money()
+	
+func update_money():
+	$Control/Control2/money.text = str(snappedf(money, 1))
